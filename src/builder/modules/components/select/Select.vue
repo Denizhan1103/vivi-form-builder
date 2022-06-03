6517ad83e24c47bf761aa5a5a3c2bc540ade1f65
Denizhan <denizhanonr2000@gmail.com>
2022-06-03 05:46:53 +0300

    Added blank select component

---
+++ src/builder/modules/components/select/Select.vue
@@ -0,0 +1,12 @@
+<template></template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Select from "./Select.ts";
+
+export default defineComponent(Select);
+</script>
+
+<style lang="scss" scoped>
+@import "./Select.scss";
+</style>
