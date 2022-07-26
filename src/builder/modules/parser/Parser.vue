00be94834ffb371ea8abb439e330cbab9e66c3c8
Denizhan Önür <denizhan@Denizhans-MacBook-Air.local>
2022-07-26 16:14:12 +0300

    Fixed parser id change problem

--- src/builder/modules/parser/Parser.vue
+++ src/builder/modules/parser/Parser.vue
@@ -1,57 +1,78 @@
 <template>
-    <div 
-        v-if="renderType == RenderType.form"
-        class="parser"
+  <div v-if="renderType == RenderType.form" class="parser">
+    <div
+      v-for="item in form.itemList.sort((a, b) => a.queue - b.queue)"
+      :key="item.id"
+      class="parser__formitem"
     >
-        <div 
-            v-for="item in form.itemList.sort((a,b) => a.queue - b.queue)" 
-            :key="item.id" 
-            class="parser__formitem"
-        >
-            <Input 
-                v-if="item.type !== 'TextArea' && item.type !== 'Select' && item.type !== 'CheckBox'"
-                :type="item.type"
-                :properties="{...item.properties, startingText: startValues[item.id] || item.properties.startingText}"
-                @onInputChanged="(newValue) => emittingObjects[`itemId${item.id}`] = newValue"
-            />
-            <TextArea 
-                v-if="item.type == 'TextArea'"
-                :properties="{...item.properties, startingText: startValues[item.id] || item.properties.startingText}"
-                @onTextAreaChanged="(newValue) => emittingObjects[`itemId${item.id}`] = newValue"
-                />
-            <Select 
-                v-if="item.type == 'Select'"
-                :properties="{...item.properties, activeValue: startValues[item.id] || item.properties.activeValue}"
-                @onChange="(newValue) => emittingObjects[`itemId${item.id}`] = newValue.id"
-            />
-            <CheckBox 
-                v-if="item.type == 'CheckBox'"
-                :properties="{...item.properties, activeValue: startValues[item.id] || item.properties.activeValue}"
-                @onActiveChanged="(newValue) => emittingObjects[`itemId${item.id}`] = newValue"
-            />
-        </div>
+      <Input
+        v-if="
+          item.type !== 'TextArea' &&
+          item.type !== 'Select' &&
+          item.type !== 'CheckBox'
+        "
+        :type="item.type"
+        :properties="{
+          ...item.properties,
+          startingText: startValues[item.id] || item.properties.startingText,
+        }"
+        @onInputChanged="(newValue) => (emittingObjects[item.id] = newValue)"
+      />
+      <TextArea
+        v-if="item.type == 'TextArea'"
+        :properties="{
+          ...item.properties,
+          startingText: startValues[item.id] || item.properties.startingText,
+        }"
+        @onTextAreaChanged="(newValue) => (emittingObjects[item.id] = newValue)"
+      />
+      <Select
+        v-if="item.type == 'Select'"
+        :properties="{
+          ...item.properties,
+          activeValue: startValues[item.id] || item.properties.activeValue,
+        }"
+        @onChange="(newValue) => (emittingObjects[item.id] = newValue.id)"
+      />
+      <CheckBox
+        v-if="item.type == 'CheckBox'"
+        :properties="{
+          ...item.properties,
+          activeValue: startValues[item.id] || item.properties.activeValue,
+        }"
+        @onActiveChanged="(newValue) => (emittingObjects[item.id] = newValue)"
+      />
     </div>
-    <ul 
-        v-if="renderType == RenderType.list"
-        class="parser"
+  </div>
+  <ul v-if="renderType == RenderType.list" class="parser">
+    <li
+      v-for="item in form.itemList.sort((a, b) => a.queue - b.queue)"
+      :key="item.id"
+      class="parser__listitem"
     >
-        <li 
-            v-for="item in form.itemList.sort((a,b) => a.queue - b.queue)" 
-            :key="item.id" 
-            class="parser__listitem"
-        >
-            <div class="parser__item-header">{{ item.properties && item.properties.header ? item.properties.header : messages.title }}:</div>
-            <span v-if="item.type == 'Select' || item.type == 'CheckBox'" class="parser__item-value"> {{ calculateValue(item) }}</span>
-            <span v-else class="parser__item-value">{{ startValues[item.id] }}</span>
-        </li>
-    </ul>
+      <div class="parser__item-header">
+        {{
+          item.properties && item.properties.header
+            ? item.properties.header
+            : messages.title
+        }}:
+      </div>
+      <span
+        v-if="item.type == 'Select' || item.type == 'CheckBox'"
+        class="parser__item-value"
+      >
+        {{ calculateValue(item) }}</span
+      >
+      <span v-else class="parser__item-value">{{ startValues[item.id] }}</span>
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import Parser from "./Parser"
+import { defineComponent } from "vue";
+import Parser from "./Parser";
 
-export default defineComponent(Parser)
+export default defineComponent(Parser);
 </script>
 
 <style lang="scss" scoped>
